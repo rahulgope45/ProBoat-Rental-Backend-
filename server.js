630891f4d7f8e085848c022a7d019e84ea361c4f
rahulgope45 <rahuleile456@gmail.com>
2025-10-01 16:04:02 +0530

    Cluster connected

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors';
+
 
 dotenv.config();
 
 const app = express();
 
 //Middleware
+app.use(cors())
+app.use(express.json())
+
+
+//Middlewares
+app.use(cors());
+app.use(express.json())
+
+//MongoDb connection
+mongoose
+        .connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+        .then(() => console.log("MongoDb connected"))
+        .catch((err) => console.error("MongoDb Error:", err))
+
+//Test Route
+app.get("/", (req, res) => {
+    res.send("Backend is running..");
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
