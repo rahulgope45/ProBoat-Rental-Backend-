868c40ec0d5ad409a93e5f1e397c4690532e4174
rahulgope45 <rahuleile456@gmail.com>
2025-10-02 23:51:12 +0530

    Auth switching started

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+
+
+
+
+export const signup = async (req,res)=>{
+
+}
+export const login = async (req,res)=>{
+    
+}
+export const logout = async (req,res)=>{
+    
+}
+
+export const checkAuth = async (req,res)=>{
+    
+}
