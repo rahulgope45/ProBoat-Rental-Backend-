c1f0f85a06145dc73a9fd00c3374c7e5973c4ac6
rahulgope45 <rahuleile456@gmail.com>
2025-10-19 22:10:51 +0530

    CheckAuth added

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -97,5 +97,13 @@ export const logout = async (req,res)=>{
 }
 
 export const checkAuth = async (req,res)=>{
+    try {
+        res.status(200).json(req.user);
+
+    } catch (error) {
+    console.log("error in Auth check controller", error.message);
+    res.status(500).json({message: "Internal server error"});
+        
+    }
     
 }
