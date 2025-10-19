c32e9e4ead2fcaf0b25011440cf323ca27c44f85
rahulgope45 <rahuleile456@gmail.com>
2025-10-19 21:50:04 +0530

    Login and Logout controllers added

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -57,9 +57,42 @@ export const signup = async (req,res)=>{
 
 }
 export const login = async (req,res)=>{
+    const { email, password} = req.body
+    try {
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({message: "invalid credentials"})
+        }
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if(!isPasswordCorrect){
+            return res.status(400).json({message: "Invalid Credentials"})
+        }
+        genrateToken(user._id,res)
+        res.status(200).json({
+         _id: user.email,
+         fullName: user.fullName,
+         email: user.email,
+        })
+    } catch (error) {
+       console.log("Error in login controller", error.message);
+    return res.status(500).json({message: "Internal server error"});
+ 
+    }
+
     
 }
 export const logout = async (req,res)=>{
+    try {
+        res.cookie("jwt", "", {maxAge: 0});
+        return res.status(200).json({
+            message: "Logout Successfully"
+        });
+    } catch (error) {
+        console.log("Error in logout controller", error.message);
+        return res.status(500).json({message: "Internal server error"});
+
+        
+    }
     
 }
 
