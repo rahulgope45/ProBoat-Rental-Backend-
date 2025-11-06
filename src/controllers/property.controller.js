d8eb79637568b8612e55974f9645c8f5b4753a73
rahulgope45 <rahuleile456@gmail.com>
2025-11-06 17:30:20 +0530

    Structure of property controller added

---
+++ src/controllers/property.controller.js
@@ -0,0 +1,58 @@
+import Property from "../models/property.model.js";
+import Review from "../models/review.model.js";
+
+
+//Controller of Listing property
+
+export const createProperty = async (req, res) =>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+//controller for get all properties
+export const getAllProperties =async (req,res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+//Get Property by id
+export const getPropertyById =async (req,res) =>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+//Update Property
+export const updateProperty =async(req, res) =>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+//Delete Property
+export const deleteProperty =async(req,res) =>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+//Get Users Properties
+export const getUserProperties = async (req,res) =>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
