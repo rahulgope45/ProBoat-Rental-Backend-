03ffc7a52de4b376530bc7ced29b51f5548efb80
rahulgope45 <rahuleile456@gmail.com>
2025-11-17 14:34:31 +0530

    Added cloudinary setrup is complete controllers will be added next

---
+++ src/controllers/upload.controller.js
@@ -0,0 +1,19 @@
+import cloudinary from "../lib/cloudinary.js";
+
+export const uploaImages = async (req,res) =>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export const deleteImage = async (req,res) =>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+
---
+++ src/middleware/uplaod.middleware.js
@@ -0,0 +1,22 @@
+import multer from "multer";
+
+const storage = multer.memoryStorage();
+
+const fileFilter = (req, file, cb) => {
+    if(file.mimetype.startsWith('image/')){
+        cb(null, true);
+
+    }else{
+        cb(new Error('Only images are allowed!'), false);
+    }
+};
+
+export const uplaod = multer({
+    storage,
+    fileFilter,
+    limits:{
+        fileSize: 2 * 1024 * 1024 //5 mb tak rakh rha hu
+    }
+
+})
+
