867a1267063c72488f946b4ad6b32b0f79011861
rahulgope45 <rahuleile456@gmail.com>
2025-11-18 16:40:59 +0530

    Added createProperty controller with this new property can be created

--- src/controllers/property.controller.js
+++ src/controllers/property.controller.js
@@ -1,13 +1,77 @@
 import Property from "../models/property.model.js";
 import Review from "../models/review.model.js";
+import cloudinary from "../lib/cloudinary.js";
 
 
 //Controller of Listing property
 
 export const createProperty = async (req, res) =>{
     try {
-        
+        const{
+            title,
+            description,
+            propertyType,
+            listingType,
+            price,
+            address,
+            location,
+            specifications,
+            amenities,
+            images,
+            owner
+
+        } = req.body;
+
+        //Validation
+        if(!title || !description || !propertyType || !listingType || !price ){
+            return res.status(400).json({
+                success: false,
+                message: 'Please provide all the required fields'
+
+            });
+        }
+        if(!images || images.length === 0){
+            return res.status(400).json({
+                success: false,
+                message: 'Please uplaod at least one image'
+            });
+        }
+
+        //Creating a property
+
+        const property = new Property({
+            title,
+            description,
+            propertyType,
+            listingType,
+            price,
+            address,location,
+            specifications,
+            amenities,
+            images,
+            owner:{
+                userId: req.user._id,
+                name: owner?.name || req.user.fullName,
+                phone: owner?.phone,
+                email: owner?.email || req.user.email
+            }
+        });
+
+        await property.save();
+
+        res.status(200).json({
+            success: true,
+            message: 'Property created succesfully',
+            property
+        });
+
+
     } catch (error) {
+        console.log('Error in creating Property:', error.message);
+        res.status(500).json({
+            success: 'false',
+            message: 'Internal server error'
+        })
         
     }
 }
