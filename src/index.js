424db1c70c858f797d18fdf6f016697235f8a4ed
rahulgope45 <rahuleile456@gmail.com>
2025-12-18 01:58:48 +0530

    Deployemnt check6

--- src/index.js
+++ src/index.js
@@ -12,12 +12,9 @@ import inquiryRoutes from './routes/inquiry.routes.js'
 
 dotenv.config();
 const app = express();
+connectDB();
 
-//Frontend
-app.use(cors({
-    origin: ["http://localhost:5173" , "https://prop-bot-rental.vercel.app"],
-    credentials: true
-}));
+
 
 
 
@@ -48,6 +45,7 @@ app.use(cors({
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
+
 
 
 
@@ -96,6 +94,8 @@ app.use((req, res) => {
 if (process.env.NODE_ENV !== 'production') {
     app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
-        connectDB();
+        
     });
 }
+
+export default app;
